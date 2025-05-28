dea26b77f3fd4248d964715ce5cf03ac8cdaff8d
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-28 12:21:18 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -12,10 +12,10 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.pothina.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.pothina.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.pothina.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.pothina.sore </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
