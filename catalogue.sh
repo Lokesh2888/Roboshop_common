69f30c8684e7cd4b62fa5288ea315955e858dc3a
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-28 11:59:09 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -5,25 +5,20 @@ app_name=catalogue
 
 check_root
 app_setup
-node_js_setup
+nodejs_setup
 systemd_setup
-
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.pothina.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.pothina.store </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
 
-print_time 
-
-
-
-
+print_time
--- common.sh
+++ common.sh
@@ -7,21 +7,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-systemd_setup(){
-    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
-    VALIDATE $? "Copying $app_name service"
-
-    systemctl daemon-reload &>>$LOG_FILE
-    systemctl enable $app_name  &>>$LOG_FILE
-    systemctl start $app_name
-    VALIDATE $? "Starting $app_name"
-}
 
 app_setup(){
     id roboshop &>>$LOG_FILE
@@ -45,7 +36,7 @@ app_setup(){
     VALIDATE $? "unzipping $app_name"
 }
 
-node_js_setup(){
+nodejs_setup(){
     dnf module disable nodejs -y &>>$LOG_FILE
     VALIDATE $? "Disabling default nodejs"
 
@@ -53,35 +44,68 @@ node_js_setup(){
     VALIDATE $? "Enabling nodejs:20"
 
     dnf install nodejs -y &>>$LOG_FILE
-    VALIDATE $? "Installing nodejs:20" 
+    VALIDATE $? "Installing nodejs:20"
 
     npm install &>>$LOG_FILE
     VALIDATE $? "Installing Dependencies"
 }
 
+maven_setup(){
+    dnf install maven -y &>>$LOG_FILE
+    VALIDATE $? "Installing Maven and Java"
+
+    mvn clean package  &>>$LOG_FILE
+    VALIDATE $? "Packaging the shipping application"
+
+    mv target/shipping-1.0.jar shipping.jar  &>>$LOG_FILE
+    VALIDATE $? "Moving and renaming Jar file"
+}
+
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+    VALIDATE $? "Install Python3 packages"
+
+    pip3 install -r requirements.txt &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+
+    cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+    VALIDATE $? "Copying payment service"
+
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Copying $app_name service"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable $app_name  &>>$LOG_FILE
+    systemctl start $app_name
+    VALIDATE $? "Starting $app_name"
+}
+
 check_root(){
     if [ $USERID -ne 0 ]
     then
-      echo -e "$R ERROR:: Please run the script using root access $N"
-      exit 1 #give other than 0 till 127 to exit the script
+        echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
     else
-      echo "You are running with root access" | tee -a $LOG_FILE
+        echo "You are running with root access" | tee -a $LOG_FILE
     fi
 }
 
-#validate function takes input as exit status, what command they tries to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-     if [ $1 -eq 0 ]
-   then
-      echo -e "$2 is.... $G SUCCESS $N" | tee -a $LOG_FILE
-   else
-      echo -e "$2 is.... $R  FAILURE $N" | tee -a $LOG_FILE
-      exit 1
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
 print_time(){
-   END_TIME=$($date +%s)
-   TOTAL_TIME=$(($END_TIME - $START_TIME))
-   echo -e "Script executed successfully. $Y Time Taken: $TOTAL_TIME seconds $N "
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(($END_TIME - $START_TIME))
+    echo -e "Script executed successfully, $Y Time taken: $TOTAL_TIME seconds $N"
 }
